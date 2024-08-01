8486ff50c8787733a8be23f23c25ec6aa1ddd888
Going Obsolete <goingobsolete@gmail.com>
2024-08-01 16:19:53 +0200

    end of day

--- sketch.js
+++ sketch.js
@@ -1,7 +1,14 @@
-let cell;
+let bgimg; // Declare variable for background image file
+let cell; // Declare variable for cell graphic frame eg the hexagon
+
+// function preload() {
+//   img = loadImage('');
+// describe('preload images and fonts');
+// }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight, P2D);
+  cell = createGraphics(windowWidth/4, windowHeight/4, WEBGL);
 }
 
 function draw() {
